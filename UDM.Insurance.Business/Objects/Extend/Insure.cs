46ae94c297d606e1518de56fe563bb39609bdedd
Tyron Ramlugan <TyronR@udm.co.za>
2021-05-19 14:24:47 +0200

    Insure Class Update

--- UDM.Insurance.Business/Objects/Extend/Insure.cs
+++ UDM.Insurance.Business/Objects/Extend/Insure.cs
@@ -1445,6 +1445,26 @@ namespace UDM.Insurance.Business
         }
         #endregion
 
+        #region Debi-Check Report
+        public static DataSet INGetDebiCheckPL(DateTime fromDate, DateTime toDate)
+        {
+            //SqlParameter[] parameters = new SqlParameter[3];
+            //parameters[0] = new SqlParameter("@CampaignID", campaignID);
+            //parameters[1] = new SqlParameter("@FromDate", _startDate.ToString("yyyy-MM-dd"));
+            //parameters[2] = new SqlParameter("@ToDate", _endDate.ToString("yyyy-MM-dd"));
+
+            //DataSet dsReducedPremiumReport = Methods.ExecuteStoredProcedure("spINReportDiary", parameters);
+
+            object param1 = Database.GetParameter("@DateFrom", fromDate);
+            object param2 = Database.GetParameter("@DateTo", toDate);
+
+            object[] paramArray = new[] { param1, param2 };
+
+            return Database.ExecuteDataSet(null, CommandType.StoredProcedure, "spReportDebiCheckPL", paramArray, 600);
+        }
+
+        #endregion
+
         #region Status Loading Screen - Specific Functionalities
 
         public static DataSet INGetStatusLoadingScreenLookups()
