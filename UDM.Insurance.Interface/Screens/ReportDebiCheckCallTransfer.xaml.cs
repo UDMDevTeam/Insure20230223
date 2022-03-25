793fe5cf3a855d65c25cc4e9ea8cdcab0ed7f523
Tyron Ramlugan <TyronR@udm.co.za>
2022-03-25 10:32:07 +0200

    Main Heading Change on DC Specialist Report

--- UDM.Insurance.Interface/Screens/ReportDebiCheckCallTransfer.xaml.cs
+++ UDM.Insurance.Interface/Screens/ReportDebiCheckCallTransfer.xaml.cs
@@ -617,6 +617,7 @@ namespace UDM.Insurance.Interface.Screens
 
             (workSheet.Cells[1, 8]).EntireColumn.NumberFormat = "##%";
             (workSheet.Cells[1, 10]).EntireColumn.NumberFormat = "##%";
+            (workSheet.Cells[1, 14]).EntireColumn.Hidden = true;
 
             (workSheet.Rows[2]).EntireRow.RowHeight = 40;
             workSheet.Rows[2].WrapText = true;
