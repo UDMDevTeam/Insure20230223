2dca514f665b77258b959a387b4f8b470961f39f
Tyron Ramlugan <TyronR@udm.co.za>
2022-06-14 11:31:20 +0200

    DebiCheck Tracking TSR sorting issue

--- UDM.Insurance.Interface/Screens/ReportDebiCheckTracker.xaml.cs
+++ UDM.Insurance.Interface/Screens/ReportDebiCheckTracker.xaml.cs
@@ -695,16 +695,16 @@ namespace UDM.Insurance.Interface.Screens
                     workSheet.Cells[totalrows, 26].Formula = string.Format("=E" + totalrows + "/(B" + totalrows + "-Y" + totalrows + ")*100"); //X
 
 
-                    //workSheet.UsedRange.Select();
-                    //workSheet.Sort.SortFields.Clear();
-                    //workSheet.Sort.SortFields.Add(workSheet.UsedRange.Columns["A"], Microsoft.Office.Interop.Excel.XlSortOn.xlSortOnValues, Microsoft.Office.Interop.Excel.XlSortOrder.xlAscending, System.Type.Missing, Microsoft.Office.Interop.Excel.XlSortDataOption.xlSortNormal);
-                    //var sort = workSheet.Sort;
-                    //sort.SetRange(workSheet.UsedRange);
-                    //sort.Header = Microsoft.Office.Interop.Excel.XlYesNoGuess.xlYes;
-                    //sort.MatchCase = false;
-                    //sort.Orientation = Microsoft.Office.Interop.Excel.XlSortOrientation.xlSortColumns;
-                    //sort.SortMethod = Microsoft.Office.Interop.Excel.XlSortMethod.xlPinYin;
-                    //sort.Apply();
+                    workSheet.UsedRange.Select();
+                    workSheet.Sort.SortFields.Clear();
+                    workSheet.Sort.SortFields.Add(workSheet.UsedRange.Columns["A"], Microsoft.Office.Interop.Excel.XlSortOn.xlSortOnValues, Microsoft.Office.Interop.Excel.XlSortOrder.xlAscending, System.Type.Missing, Microsoft.Office.Interop.Excel.XlSortDataOption.xlSortNormal);
+                    var sort = workSheet.Sort;
+                    sort.SetRange(workSheet.UsedRange);
+                    sort.Header = Microsoft.Office.Interop.Excel.XlYesNoGuess.xlYes;
+                    sort.MatchCase = false;
+                    sort.Orientation = Microsoft.Office.Interop.Excel.XlSortOrientation.xlSortColumns;
+                    sort.SortMethod = Microsoft.Office.Interop.Excel.XlSortMethod.xlPinYin;
+                    sort.Apply();
 
                     for (int w = 3; w <= totalRowMinusOne; w++)
                     {
