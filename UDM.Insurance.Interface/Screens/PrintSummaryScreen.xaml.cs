1545fc7623cad8bcc83ada4318b10eace0ef6838
Dane Burts <DaneB@udm.co.za>
2022-06-22 08:33:05 +0200

    DMM9U Print Format Change

--- UDM.Insurance.Interface/Screens/PrintSummaryScreen.xaml.cs
+++ UDM.Insurance.Interface/Screens/PrintSummaryScreen.xaml.cs
@@ -470,7 +470,7 @@ namespace UDM.Insurance.Interface.Screens
 
                             leadTemplateRowIndex = 40;
                         }
-                        else if (campaign.Code == "PLDMM6U" || campaign.Code == "PLDMM7U" || campaign.Code == "PLDMM8U")
+                        else if (campaign.Code == "PLDMM6U" || campaign.Code == "PLDMM7U" || campaign.Code == "PLDMM8U" || campaign.Code == "PLDMM9U")
                         {
                             uri = new Uri("/Templates/PrintTemplateUpgrade5.xlsx", UriKind.Relative);
                             TemplateLines = 15;//13;
@@ -613,7 +613,7 @@ namespace UDM.Insurance.Interface.Screens
 
                     if (IsUpgradeCampaign)
                     {
-                        if (campaign.Code == "PLDMM5U" || campaign.Code == "PLDMM8U" || campaign.Code == "PLDMM7U" || campaign.Code == "PLDMM6U")
+                        if (campaign.Code == "PLDMM5U" || campaign.Code == "PLDMM8U" || campaign.Code == "PLDMM7U" || campaign.Code == "PLDMM6U" || campaign.Code == "PLDMM9U")
                         {
                             ds = Methods.ExecuteStoredProcedure("spINGetLeadsForUserAndBatchPLDMM5U", parameters);
                         }
