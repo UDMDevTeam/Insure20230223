523c5ba7f4a0f894d083954e925c39d3d5183040
Tyron Ramlugan <TyronR@udm.co.za>
2022-11-11 09:06:51 +0200

    Redeemed Gift keeps breaking

--- UDM.Insurance.Interface/Screens/RedeemGiftScreen.xaml.cs
+++ UDM.Insurance.Interface/Screens/RedeemGiftScreen.xaml.cs
@@ -41,28 +41,35 @@ namespace UDM.Insurance.Interface.Screens
         {
             InitializeComponent();
 
-            _userData = LaData.UserData;
-            _appData = LaData.AppData;
-
-            LoadLookupData(LaData.AppData.ImportID);
-            LoadScreenData(LaData.AppData.ImportID);
-
             try
             {
-                string GiftRedeemed = Convert.ToString(Methods.GetTableData("SELECT Gift FROM INImport WHERE ID = " + LaData.AppData.ImportID).Rows[0][0]);
+                _userData = LaData.UserData;
+                _appData = LaData.AppData;
 
-                if (GiftRedeemed == "")
+                LoadLookupData(LaData.AppData.ImportID);
+                LoadScreenData(LaData.AppData.ImportID);
+
+                try
+                {
+                    string GiftRedeemed = Convert.ToString(Methods.GetTableData("SELECT Gift FROM INImport WHERE ID = " + LaData.AppData.ImportID).Rows[0][0]);
+
+                    if (GiftRedeemed == "")
+                    {
+                        medRedeemedGift.Text = "Gift N/A";
+                    }
+                    else
+                    {
+                        medRedeemedGift.Text = "Gift Redeemed - " + GiftRedeemed + ".";
+                    }
+                }
+                catch
                 {
                     medRedeemedGift.Text = "Gift N/A";
-                }
-                else
-                {
-                    medRedeemedGift.Text = "Gift Redeemed - " + GiftRedeemed + ".";
                 }
             }
             catch
             {
-                medRedeemedGift.Text = "Gift N/A";
+
             }
         }
 
@@ -79,77 +86,97 @@ namespace UDM.Insurance.Interface.Screens
             //strSQL = "SELECT ID, Gift FROM INGiftOption WHERE GetDate() BETWEEN ActiveStartDate AND ActiveEndDate";
             //DataTable dtGiftOption = Methods.GetTableData(strSQL);
             //cmbGiftSelection.Populate(dtGiftOption, "Gift", "ID");
-
-            if (fkINImportID.HasValue)
+            try
             {
-                DataSet dsLookups = Insure.INGetRedeemGiftScreenLookups(fkINImportID.Value);
-                cmbGiftStatus.Populate(dsLookups.Tables[0], "Description", "ID");
-                cmbGiftSelection.Populate(dsLookups.Tables[1], "Gift", "ID");
-                cmbRedeemedBy.Populate(dsLookups.Tables[2], "User", "ID");
+                if (fkINImportID.HasValue)
+                {
+                    DataSet dsLookups = Insure.INGetRedeemGiftScreenLookups(fkINImportID.Value);
+                    cmbGiftStatus.Populate(dsLookups.Tables[0], "Description", "ID");
+                    cmbGiftSelection.Populate(dsLookups.Tables[1], "Gift", "ID");
+                    cmbRedeemedBy.Populate(dsLookups.Tables[2], "User", "ID");
+                }
             }
+            catch
+            {
+
+            }
+
         }
 
         private void LoadScreenData(long? importID)
         {
-            INGiftRedeem inGiftRedeem = INGiftRedeemMapper.SearchOne(importID, null);
-
-            if(inGiftRedeem == null)
+            try
             {
-                inGiftRedeem = new INGiftRedeem();
-                inGiftRedeem.FKlkpGiftRedeemStatusID = 2;
-                inGiftRedeem.Save(null);
-            }
+                INGiftRedeem inGiftRedeem = INGiftRedeemMapper.SearchOne(importID, null);
 
-            ScreenData.GiftRedeemID = inGiftRedeem.ID;
-            ScreenData.ImportID = importID;
-            ScreenData.GiftRedeemStatusID = inGiftRedeem.FKlkpGiftRedeemStatusID;
-            ScreenData.GiftRedeemStatus = (lkpINGiftRedeemStatus?) inGiftRedeem.FKlkpGiftRedeemStatusID;
-            ScreenData.GiftOptionID = inGiftRedeem.FKGiftOptionID;
-            ScreenData.DateRedeemed = inGiftRedeem.RedeemedDate;
-            ScreenData.PODDate = inGiftRedeem.PODDate;
-            ScreenData.PODSignature = inGiftRedeem.PODSignature;
-            ScreenData.IsWebRedeemed = inGiftRedeem.IsWebRedeemed;
-            ScreenData.IsRedeemedGiftFieldModifiable = Insure.IsRedeemedGiftFieldsModifiable();
-            ScreenData.CanEditDetails = Insure.INCanUserEditRedeemedGiftDetails(importID);
-
-            if (inGiftRedeem.FKUserID == null)
-            {
-                if (_userData.UserType == lkpUserType.SalesAgent)
+                if (inGiftRedeem == null)
                 {
-                    ScreenData.FKUserID = _userData.UserID;
+                    inGiftRedeem = new INGiftRedeem();
+                    inGiftRedeem.FKlkpGiftRedeemStatusID = 2;
+                    inGiftRedeem.Save(null);
                 }
-                else if (_appData.AgentID != null)
+
+                ScreenData.GiftRedeemID = inGiftRedeem.ID;
+                ScreenData.ImportID = importID;
+                ScreenData.GiftRedeemStatusID = inGiftRedeem.FKlkpGiftRedeemStatusID;
+                ScreenData.GiftRedeemStatus = (lkpINGiftRedeemStatus?)inGiftRedeem.FKlkpGiftRedeemStatusID;
+                ScreenData.GiftOptionID = inGiftRedeem.FKGiftOptionID;
+                ScreenData.DateRedeemed = inGiftRedeem.RedeemedDate;
+                ScreenData.PODDate = inGiftRedeem.PODDate;
+                ScreenData.PODSignature = inGiftRedeem.PODSignature;
+                ScreenData.IsWebRedeemed = inGiftRedeem.IsWebRedeemed;
+                ScreenData.IsRedeemedGiftFieldModifiable = Insure.IsRedeemedGiftFieldsModifiable();
+                ScreenData.CanEditDetails = Insure.INCanUserEditRedeemedGiftDetails(importID);
+
+                if (inGiftRedeem.FKUserID == null)
                 {
-                    ScreenData.FKUserID = _appData.AgentID;
+                    if (_userData.UserType == lkpUserType.SalesAgent)
+                    {
+                        ScreenData.FKUserID = _userData.UserID;
+                    }
+                    else if (_appData.AgentID != null)
+                    {
+                        ScreenData.FKUserID = _appData.AgentID;
+                    }
+                    else
+                    {
+                        ScreenData.FKUserID = null;
+                    }
                 }
                 else
                 {
-                    ScreenData.FKUserID = null;
+                    ScreenData.FKUserID = inGiftRedeem.FKUserID;
                 }
-            }
-            else
-            {
-                ScreenData.FKUserID = inGiftRedeem.FKUserID;
-            }
-            
 
-            //Defaults
-            if (ScreenData.GiftRedeemStatusID == null)
-            {
-                ScreenData.GiftRedeemStatusID = 2;
-            }
 
-            if (ScreenData.DateRedeemed == null && ScreenData.GiftRedeemStatusID == (long?)lkpINGiftRedeemStatus.NotRedeemed)
-            {
-                ScreenData.DateRedeemed = DateTime.Now;
-            }
+                //Defaults
+                if (ScreenData.GiftRedeemStatusID == null)
+                {
+                    ScreenData.GiftRedeemStatusID = 2;
+                }
 
-            ScreenData.PreviousRedeemStatus = ScreenData.GiftRedeemStatusID;
+                if (ScreenData.DateRedeemed == null && ScreenData.GiftRedeemStatusID == (long?)lkpINGiftRedeemStatus.NotRedeemed)
+                {
+                    ScreenData.DateRedeemed = DateTime.Now;
+                }
 
-            //From 2018-09-17 only the "R500 Gift Card" option should show for NR batches with leads that have not yet been redeemed.
-            if (ScreenData.GiftRedeemStatusID == 2)
-            {
-                if (DateTime.Now >= Convert.ToDateTime("2018-09-17 00:00:00"))
+                ScreenData.PreviousRedeemStatus = ScreenData.GiftRedeemStatusID;
+
+                //From 2018-09-17 only the "R500 Gift Card" option should show for NR batches with leads that have not yet been redeemed.
+                if (ScreenData.GiftRedeemStatusID == 2)
+                {
+                    if (DateTime.Now >= Convert.ToDateTime("2018-09-17 00:00:00"))
+                    {
+                        string strSQL = "SELECT ID, Gift FROM INGiftOption WHERE ID = 12";
+
+                        DataTable dtGifts = Methods.GetTableData(strSQL);
+
+                        cmbGiftSelection.Populate(dtGifts, "Gift", "ID");
+
+                        ScreenData.GiftOptionID = 12;
+                    }
+                }
+                else if (ScreenData.GiftRedeemStatusID == 1 && ScreenData.DateRedeemed >= Convert.ToDateTime("2018-09-17 00:00:00"))
                 {
                     string strSQL = "SELECT ID, Gift FROM INGiftOption WHERE ID = 12";
 
@@ -157,20 +184,14 @@ namespace UDM.Insurance.Interface.Screens
 
                     cmbGiftSelection.Populate(dtGifts, "Gift", "ID");
 
-                    ScreenData.GiftOptionID = 12;
+                    //ScreenData.GiftOptionID = 12;
+
+                    cmbGiftSelection.SelectedIndex = 0;
                 }
             }
-            else if (ScreenData.GiftRedeemStatusID == 1 && ScreenData.DateRedeemed >= Convert.ToDateTime("2018-09-17 00:00:00"))
+            catch
             {
-                string strSQL = "SELECT ID, Gift FROM INGiftOption WHERE ID = 12";
 
-                DataTable dtGifts = Methods.GetTableData(strSQL);
-
-                cmbGiftSelection.Populate(dtGifts, "Gift", "ID");
-
-                //ScreenData.GiftOptionID = 12;
-
-                cmbGiftSelection.SelectedIndex = 0;
             }
         }
 
@@ -242,53 +263,69 @@ namespace UDM.Insurance.Interface.Screens
         {
             //INMessageBoxWindow2 inMessageBox = new INMessageBoxWindow2(null, "Are you sure you want to save the redeem gift information?", "Redeem Gift", ShowMessageType.Exclamation);
             //var result = inMessageBox.ShowDialog();
-
-            MessageBoxResult result = MessageBox.Show("Are you sure the redeem gift information is correct?", "Redeem Gift", MessageBoxButton.YesNo, MessageBoxImage.Exclamation);
-
-            if (result == MessageBoxResult.Yes)
+            try
             {
-                if (ScreenData.GiftRedeemID != null)
+                MessageBoxResult result = MessageBox.Show("Are you sure the redeem gift information is correct?", "Redeem Gift", MessageBoxButton.YesNo, MessageBoxImage.Exclamation);
+
+                if (result == MessageBoxResult.Yes)
                 {
-                    INGiftRedeem inGiftRedeem = new INGiftRedeem((long) ScreenData.GiftRedeemID);
+                    if (ScreenData.GiftRedeemID != null)
+                    {
+                        INGiftRedeem inGiftRedeem = new INGiftRedeem((long)ScreenData.GiftRedeemID);
 
-                    inGiftRedeem.FKINImportID = ScreenData.ImportID;
-                    inGiftRedeem.FKlkpGiftRedeemStatusID = ScreenData.GiftRedeemStatusID = 1;
-                    inGiftRedeem.FKGiftOptionID = ScreenData.GiftOptionID;
-                    inGiftRedeem.RedeemedDate = ScreenData.DateRedeemed;
-                    //inGiftRedeem.PODDate = ScreenData.PODDate;
-                    //inGiftRedeem.PODSignature = ScreenData.PODSignature;
+                        inGiftRedeem.FKINImportID = ScreenData.ImportID;
+                        inGiftRedeem.FKlkpGiftRedeemStatusID = ScreenData.GiftRedeemStatusID = 1;
+                        inGiftRedeem.FKGiftOptionID = ScreenData.GiftOptionID;
+                        inGiftRedeem.RedeemedDate = ScreenData.DateRedeemed;
+                        //inGiftRedeem.PODDate = ScreenData.PODDate;
+                        //inGiftRedeem.PODSignature = ScreenData.PODSignature;
 
-                    inGiftRedeem.IsWebRedeemed = ScreenData.IsWebRedeemed;
-                    inGiftRedeem.FKUserID = ScreenData.FKUserID;
+                        inGiftRedeem.IsWebRedeemed = ScreenData.IsWebRedeemed;
+                        inGiftRedeem.FKUserID = ScreenData.FKUserID;
 
-                    inGiftRedeem.Save(null);
+                        inGiftRedeem.Save(null);
+                    }
                 }
             }
+            catch
+            {
+
+            }
+
+
         }
 
         private void btnEdit_Click(object sender, RoutedEventArgs e)
         {
-            MessageBoxResult result = MessageBox.Show("Are you sure the redeem gift information is correct?", "Confirm Details", MessageBoxButton.YesNo, MessageBoxImage.Exclamation);
-
-            if (result == MessageBoxResult.Yes)
+            try
             {
-                if (ScreenData.GiftRedeemID != null)
+                MessageBoxResult result = MessageBox.Show("Are you sure the redeem gift information is correct?", "Confirm Details", MessageBoxButton.YesNo, MessageBoxImage.Exclamation);
+
+                if (result == MessageBoxResult.Yes)
                 {
-                    INGiftRedeem inGiftRedeem = new INGiftRedeem((long)ScreenData.GiftRedeemID);
+                    if (ScreenData.GiftRedeemID != null)
+                    {
+                        INGiftRedeem inGiftRedeem = new INGiftRedeem((long)ScreenData.GiftRedeemID);
 
-                    inGiftRedeem.FKINImportID = ScreenData.ImportID;
-                    inGiftRedeem.FKlkpGiftRedeemStatusID = ScreenData.GiftRedeemStatusID;
-                    inGiftRedeem.FKGiftOptionID = ScreenData.GiftOptionID;
-                    inGiftRedeem.RedeemedDate = ScreenData.DateRedeemed;
-                    //inGiftRedeem.PODDate = ScreenData.PODDate;
-                    //inGiftRedeem.PODSignature = ScreenData.PODSignature;
+                        inGiftRedeem.FKINImportID = ScreenData.ImportID;
+                        inGiftRedeem.FKlkpGiftRedeemStatusID = ScreenData.GiftRedeemStatusID;
+                        inGiftRedeem.FKGiftOptionID = ScreenData.GiftOptionID;
+                        inGiftRedeem.RedeemedDate = ScreenData.DateRedeemed;
+                        //inGiftRedeem.PODDate = ScreenData.PODDate;
+                        //inGiftRedeem.PODSignature = ScreenData.PODSignature;
 
-                    inGiftRedeem.IsWebRedeemed = ScreenData.IsWebRedeemed;
-                    inGiftRedeem.FKUserID = ScreenData.FKUserID;
+                        inGiftRedeem.IsWebRedeemed = ScreenData.IsWebRedeemed;
+                        inGiftRedeem.FKUserID = ScreenData.FKUserID;
 
-                    inGiftRedeem.Save(null);
+                        inGiftRedeem.Save(null);
+                    }
                 }
             }
+            catch
+            {
+
+            }
+
         }
 
         public void EmbriantComboBox_PreviewKeyDown(object sender, KeyEventArgs e)
