26e41d23ad1222b9845d8fc766d0f9044e7505ab
TyronR <tyronr@udm.co.za>
2023-09-22 14:05:35 +0200

    Cancer referral campaign update to auto fill the cancer option

--- UDM.Insurance.Interface/Screens/LeadApplicationScreen.xaml.cs
+++ UDM.Insurance.Interface/Screens/LeadApplicationScreen.xaml.cs
@@ -8470,7 +8470,7 @@ namespace UDM.Insurance.Interface.Screens
 
                 #region PLCBSpouse Campaign Auto CancerOption Update
 
-                if (LaData.AppData.CampaignID == 344 || LaData.AppData.CampaignType == lkpINCampaignType.BlackMaccMillion && LaData.AppData.CampaignGroup == lkpINCampaignGroup.Upgrade5 || LaData.AppData.CampaignType == lkpINCampaignType.BlackMaccMillion && LaData.AppData.CampaignGroup == lkpINCampaignGroup.Upgrade6)
+                if (LaData.AppData.CampaignID == 344 || LaData.AppData.CampaignID == 423 || LaData.AppData.CampaignType == lkpINCampaignType.BlackMaccMillion && LaData.AppData.CampaignGroup == lkpINCampaignGroup.Upgrade5 || LaData.AppData.CampaignType == lkpINCampaignType.BlackMaccMillion && LaData.AppData.CampaignGroup == lkpINCampaignGroup.Upgrade6)
                 {
                     decimal? selectedLA1Cover = LaData.PolicyData.LA1Cover;
                     int age = DateTime.Now.Year - date.Year;
